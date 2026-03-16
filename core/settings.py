a4b8d58f116ec27d895a085d390a9636ca25be3c
Scorchius-dev <ahmed.alaamari01@gmail.com>
2026-03-16 11:32:05 +0000

    feat: Add WhiteNoise middleware for static file handling and update requirements

--- core/settings.py
+++ core/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -109,8 +110,11 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = (
+    'whitenoise.storage.CompressedManifestStaticFilesStorage'
+)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # --- API Configuration (Hiding your keys) ---
